bed9695f70d770b968cdad4eb9b0768b4eed5672
Irina Yurkevich <i.yurkevich@softteco.com>
2019-08-13 14:28:10 +0300

    created graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import LineChart from './components/LineChart/LineChart';
-import { data, defaultProps } from './components/LineChart/constants';
+import { data, defaultLineChartProps } from './components/LineChart/constants';
 
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-        <LineChart {...defaultProps} data={data} width={800} height={600} className="test-chart" />
+        <LineChart {...defaultLineChartProps} data={data} width={800} height={600} className="test-chart" />
     </div>
   );
 }
--- src/components/LineChart/utils.ts
+++ src/components/LineChart/utils.ts
@@ -1,7 +1,11 @@
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 export const createAxes = (props: any) => {
-    const { width, height, className, data, plotIndents } = props;
+    const {
+        width, height, className, data, plotIndents,
+        lineColor, lineWidth,
+        dotsColor, dotsFill, dotsWidth,
+    } = props;
 
     const svg = d3.select(`.${className}`);
 
@@ -17,11 +21,48 @@ export const createAxes = (props: any) => {
 
     const xPlotGroup = svg.append('g')
         .classed('x-axis', true)
-        .attr("transform", `translate(${plotIndents.left},${height - plotIndents.left})`)
+        .attr('transform', `translate(${plotIndents.left},${height - plotIndents.left})`)
         .call(xAxis);
 
     const yPlotGroup = svg.append('g')
         .classed('y-axis', true)
-        .attr("transform", `translate(${plotIndents.top},${plotIndents.bottom})`)
+        .attr('transform', `translate(${plotIndents.top},${plotIndents.bottom})`)
         .call(yAxis);
+
+
+    const sortByX = function (a: any, b: any) {
+        return a.x < b.x?-1:1;
+    };
+
+    // scale data to our coordinate system
+    const scaledData = data.map((item: any, i: number) => ({
+        x: xScale(data[i].x) + plotIndents.left,
+        y: yScale(data[i].y) + plotIndents.left,
+    })).sort(sortByX);
+
+
+    const line = d3.line()
+        .x(function(d: any){return d.x;})
+        .y(function(d: any){return d.y;})
+        .curve(d3.curveCatmullRom.alpha(0.5));
+
+    svg.append('g').append('path')
+        .datum(scaledData)
+        .attr('d', line)
+        .classed('line', true)
+        .style('fill', 'none')
+        .style('stroke', lineColor)
+        .style('stroke-width', lineWidth);
+
+    svg.selectAll('.dot')
+        .data(data)
+        .enter().append('circle')
+        .classed('dot', true)
+        .style('fill', dotsFill)
+        .style('stroke', dotsColor)
+        .style('stroke-width', dotsWidth)
+        .attr('r', 4)
+        .attr('cx', function(d: any) { return xScale(d.x) + plotIndents.left; })
+        .attr('cy', function(d: any) { return yScale(d.y) + plotIndents.top; });
+
 };
