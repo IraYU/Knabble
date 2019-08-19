9ada6bd65ce83395c86065f39516ec68a6ea284a
Irina Yurkevich <i.yurkevich@softteco.com>
2019-08-19 14:49:54 +0300

    corrected types

--- src/components/LineChart/LineChart.test.tsx
+++ src/components/LineChart/LineChart.test.tsx
@@ -1,30 +1,87 @@
 import React from 'react';
 import * as d3 from 'd3';
-//import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+//import { toHaveAttribute } from ''
+//import '@testing-library/jest-dom/extend-expect';
+//import renderer from 'react-test-renderer';
+//import { wrapper } from 'enzyme';
 import LineChart from "./LineChart";
 import { data, defaultLineChartProps } from "./constants";
-
+import { createLineChart } from "./utils";
 
 const testProps = {
     ...defaultLineChartProps,
-    data:data,
+    data: data,
     width:800,
     height:600,
     sortData:true,
     yAxisMax:1,
 };
 
+
+
 const svg = d3.select('svg');
 
 describe('create component', () => {
 
     it('renders without crashing', () => {
-        const div = document.createElement('div');
+        const svg = document.createElement('svg');
 
         ReactDOM.render(
-            <LineChart {...testProps} />, div,
+            <LineChart {...testProps} />, svg,
         );
+        ReactDOM.unmountComponentAtNode(svg);
+    });
+
+/*    it('renders correctly', () => {
+        const button = renderer.create(
+            (<LineChart {...testProps} />),
+        ).toJSON();
+
+        expect(button).toMatchSnapshot();
+    });*/
+
+/*
+    it('render a chart with minimal requirements', function(){
+
+/!*        const chart = renderer.create(
+            (<LineChart {...testProps} />),
+        ).toJSON();
+
+        console.log(chart);*!/
+        expect(svg.node()).toHaveAttribute('width','800')
+
+       // expect(('.dots').toBeFalsy();
+    });
+*/
+
+/*    it('should be created', function() {
+        expect(svg).not.toBeNull();
+    });*/
+
+/*    it('should have the correct height', function() {
+        expect(svg.attr('height')).toBe('600');
+    });
+
+    it('should have the correct width', function() {
+        expect(svg.attr('width')).toBe('800');
+    });*/
+
+});
+
+/*
+describe('create component 2', () => {
+
+    let c;
+
+    beforeEach(function() {
+        c = createLineChart(testProps);
+    });
+
+    afterEach(function() {
+        d3.select('svg').remove();
     });
 
     it('should be created', function() {
@@ -40,36 +97,45 @@ describe('create component', () => {
     });
 
 });
+*/
 
+/*
 describe('test Axis creation', function() {
-    it('should create a xAxis', function() {
-        expect(d3.select('.axis-x').nodes().length).toBe(1);
-    });
 
     it('should create a yAxis', function() {
         expect(d3.select('.axis-y').nodes().length).toBe(1);
     });
 });
+*/
 
 describe('create chart' ,function() {
 
-    it('should be created path', function() {
+/*    it('should be created path', function() {
         expect(svg.select('.line')).not.toBeNull();
-    });
+    });*/
 
     it('should render the correct number of dots', function() {
-        expect(svg.selectAll('.dot').nodes().length).toBe(testProps.data.length);
+        //expect(svg.selectAll('.dot').nodes().length).toBe(testProps.data.length);
+
+        ///createLineChart(testProps);
+
+
+        //container.datum(dataset).call(barChart);
+        const svg = createLineChart(testProps);
+
+        //svg.datum(data).call(createLineChart(testProps));
+
+
+
+        //const wrapper = render(<LineChart {...testProps} />);
+        const wrapper = render(<svg />);
+
+        //console.log(wrapper.debug());
+
+       // expect(d3.selectAll('dot').toBe(testProps.data.length);
+
+        expect(wrapper.find('.dot')).toBe(testProps.data.length);
+        //expect(wrapper).toMatchSnapshot();
+
     });
-
-/*
-
-    it('Test click event', () => {
-        const mockCallBack = jest.fn();
-        const button = shallow(
-            (<Button {...mockProps} onClick={mockCallBack}>My lovely button</Button>),
-        );
-
-        button.find('Button').simulate('click');
-        expect(mockCallBack.mock.calls.length).toEqual(1);
-    });*/
 });
--- src/components/LineChart/types.ts
+++ src/components/LineChart/types.ts
@@ -1,3 +1,22 @@
+export type Props = {
+    width: number,
+    height: number,
+    className: string,
+    data: Data,
+    sortData: boolean,
+    yAxisMax: number,
+    plotIndents: plotIndents,
+    dotsRadius: number,
+    dotsStrokeWidth: number,
+};
+
+export type plotIndents = {
+    left: number,
+    right: number,
+    top: number,
+    bottom: number,
+};
+
 export type Data = Point[];
 
 export type Point = [number, number];
